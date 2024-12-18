bb33ba60c016220775b6b34133703ee1abaaa6a1
Robots Dance <97304205+robots-dance@users.noreply.github.com>
2024-12-17 20:38:00 -0600

    Create messages.h

---
+++ wgts/messages.h
@@ -0,0 +1,12 @@
+#ifndef _MESSAGES_H_
+#define _MESSAGES_H_
+
+#define FILE_WASNT_OPENED "File wasn't opened!"
+#define ENC_PARAMS_ARE_BAD "Parameters for a encryption are incorrect!"
+#define TABLE_ISNT_LOADED "Table isn't loaded!"
+#define TREE_ISNT_LOADED "Tree isn't loaded!"
+
+#define DEF_TITLE( value ) const QString _PK_title = value;
+#define GET_TITLE() _PK_title
+
+#endif // _MESSAGES_H_
