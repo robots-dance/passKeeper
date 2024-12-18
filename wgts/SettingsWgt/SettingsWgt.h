2d7a62ba312a8a5bd85f420cfc3784be465bf13a
Robots Dance <97304205+robots-dance@users.noreply.github.com>
2024-12-17 20:57:59 -0600

    Add files via upload

---
+++ wgts/SettingsWgt/SettingsWgt.h
@@ -0,0 +1,20 @@
+#ifndef _SETTINGS_WIDGET_H_
+#define _SETTINGS_WIDGET_H_
+
+#include <QWidget>
+
+class SettingsWgt: public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit SettingsWgt( QWidget *parent = 0 );
+
+signals:
+
+public slots:
+
+};
+
+#endif // _SETTINGS_WIDGET_H_
+
