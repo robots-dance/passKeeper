49a006df52ca66115bee6bd3ce6903231f3040aa
Robots Dance <97304205+robots-dance@users.noreply.github.com>
2024-12-17 21:00:36 -0600

    Add files via upload

---
+++ wgts/TemplWgt/TemplWgt.h
@@ -0,0 +1,19 @@
+#ifndef _TEMPLATE_WIDGET_H_
+#define _TEMPLATE_WIDGET_H_
+
+#include <QWidget>
+
+class TemplWgt: public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit TemplWgt( QWidget *parent = 0 );
+
+signals:
+
+public slots:
+};
+
+#endif // _TEMPLATE_WIDGET_H_
+
