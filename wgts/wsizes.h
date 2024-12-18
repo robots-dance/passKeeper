43cd3691e79fdaa74498f577805655e9b094ff1b
Robots Dance <97304205+robots-dance@users.noreply.github.com>
2024-12-17 20:39:10 -0600

    Add files via upload

---
+++ wgts/wsizes.h
@@ -0,0 +1,25 @@
+#ifndef _WIN_SIZES_H_
+#define _WIN_SIZES_H_
+
+// AboutDlg
+#define ABOUT_HEIGHT 270
+#define ABOUT_WIDTH 300
+
+// AuthWgt
+#define AUTH_HEIGHT 200
+#define AUTH_WIDTH 270
+
+// DescrWgt
+#define DESCR_HEIGHT 380
+#define DESCR_WIDTH 300
+
+// NewKeyWgt
+#define NK_HEIGHT 180
+#define NK_WIDTH 270
+
+// MainWgt
+#define WIN_HEIGHT 400
+#define WIN_WIDTH 600
+
+#endif // _WIN_SIZES_H_
+
